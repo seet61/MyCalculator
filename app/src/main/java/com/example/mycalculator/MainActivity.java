69e73d622829e8be5a0f40b737007939ccb25513
seet61 <seet61@gmail.com>
2017-05-09 15:08:03 +0300

    example is done

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mycalculator;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -46,5 +48,66 @@ public class MainActivity extends AppCompatActivity {
         operationField.setText(lastOperation);
     }
 
+    //Обработчик нажатия кнопок цифр
+    public void onNumberClick(View view){
+        Button button = (Button) view;
+        numberField.append(button.getText());
 
+        if (lastOperation.equals("=") && operand != null) {
+            operand = null;
+        }
+    }
+
+    //Обработчик нажатия кнопок операций
+    public void onOperationClick(View view) {
+        Button button = (Button) view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+        //Если что-нибудь введено
+        if (number.length()>0) {
+            number = number.replace(",", ".");
+            try {
+                performOperation(Double.valueOf(number), op);
+            } catch (NumberFormatException ex){
+                numberField.setText("Ошибка!");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+
+    //Операция выполнения арифметики
+    private void performOperation (Double number, String operation){
+        //Если операнд ранее не установлен
+        if (operand == null){
+            operand = number;
+        } else {
+            if (lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch (lastOperation) {
+                case "=":
+                    operand = number;
+                    break;
+                case "/":
+                    if (number ==0) {
+                        operand = 0.0;
+                    } else {
+                        operand /= number;
+                    }
+                    break;
+                case "*":
+                    operand *= number;
+                    break;
+                case "+":
+                    operand += number;
+                    break;
+                case "-":
+                    operand -= number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace(".", ","));
+        numberField.setText("");
+    }
 }
